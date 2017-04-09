2996efa98a64521b29f5c2bbeb23049c82828be7
Luke Harris <luke.harris@student.centralia.edu>
2017-04-09 13:23:03 -0700

    adding content to home controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -24,7 +24,8 @@ module.exports.homelist = function(req, res){
 			rating: 2,
 			facilities: ['Food', 'Premium Wifi'],
 			distance: '250m'
-		}]
+		}],
+		sidebar : "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for."
 	});
 };
 
